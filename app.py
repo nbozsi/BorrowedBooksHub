64048e2443c0830c1e7c53696f21a8f9eb7054e4
nbozsi <nbozsi4@gmail.com>
2023-10-12 14:49:32 +0200

    init

---
+++ app.py
@@ -0,0 +1,93 @@
+from fastapi import FastAPI, Request, Depends, Form, status
+from fastapi.staticfiles import StaticFiles
+
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
+
+from sqlalchemy.orm import Session
+from sqlalchemy import and_, func
+
+import models
+from database import SessionLocal, engine
+import json
+
+
+with open("./lang/hu.json", "r", encoding="utf-8") as f:
+    lang = json.load(f)
+
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+# Dependency
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def home(request: Request, db: Session = Depends(get_db)):
+    books = db.query(models.Book).order_by(models.Book.id.desc()).all()
+    return templates.TemplateResponse("base.html", {"request": request, "lang": lang, "book_list": books})
+
+
+@app.get("/new")
+def new(request: Request, db: Session = Depends(get_db)):
+    return templates.TemplateResponse("new.html", {"request": request, "lang": lang})
+
+
+@app.post("/add")
+def add(request: Request, title: str = Form(None), author: str = Form("Ismeretlen szerző"), db: Session = Depends(get_db)):
+    if not title:
+        url = app.url_path_for("new")
+        return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+
+    new_book = models.Book(title=title, author=author)
+    db.add(new_book)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
+
+
+@app.get("/change/{book_id}")
+def change(request: Request, book_id: int, db: Session = Depends(get_db)):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    return templates.TemplateResponse("change.html", {"request": request, "lang": lang, "book": book})
+
+
+@app.post("/update/{book_id}")
+def update(request: Request, book_id: int, author: str = Form(...), title: str = Form(...), db: Session = Depends(get_db)):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    book.title = title
+    book.author = author
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+
+@app.get("/delete/{book_id}")
+def delete(request: Request, book_id: int, db: Session = Depends(get_db)):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    db.delete(book)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
+
+
+@app.post('/search')
+def search(request: Request, title: str = Form(""), author: str = Form(""), db: Session = Depends(get_db)):
+
+    books = db.query(models.Book).filter(
+        and_(func.lower(models.Book.title).contains(title.lower()), func.lower(models.Book.author).contains(author.lower()))).all()
+    return templates.TemplateResponse("base.html", {"request": request, "lang": lang, "book_list": books})
---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import DateTime, Column, Integer, String, Boolean
+
+from database import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    author = Column(String)

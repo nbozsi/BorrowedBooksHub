9a8bf75f9b1596affc7a88c341fb5eb47946c8fa
nbozsi <nbozsi4@gmail.com>
2023-11-04 14:15:18 +0100

    searchpage -> base

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def search(request: Request, title: str, author: str, renter: str, db: Session =
         books = books.filter(func.lower(
             models.Book.renter).contains(renter.lower()))
 
-    return templates.TemplateResponse("searchpage.html", {"request": request, "lang": lang, "author": author, "title": title, "renter": renter, "book_list": books.order_by(models.Book.author).all()})
+    return templates.TemplateResponse("base.html", {"request": request, "lang": lang, "author": author, "title": title, "renter": renter, "book_list": books.order_by(models.Book.author).all()})
 
 
 @app.get('/export')
